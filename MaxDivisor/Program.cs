cb7622df8e634e882f53e416d1289b451ebca0f3
orlovamaria <arlova.maryia@gmail.com>
2021-08-22 00:41:48 +0300

    Add project files.

---
+++ MaxDivisor/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MaxDivisor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int b = 5000; ; b--)
+            {
+                if (b % 39 == 0)
+                {
+                    Console.WriteLine($"The maximum number divisible by 39 is {b}");
+                    break;
+                }
+           
+            }
+        }
+    }
+}
